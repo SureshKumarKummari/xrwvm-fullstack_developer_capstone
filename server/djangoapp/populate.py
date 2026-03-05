59117024748644057aab190c16bdaf4dcdce4d79
Suresh Kumar <96344350+SureshKumarKummari@users.noreply.github.com>
2026-03-05 17:03:44 +0530

    cmment

--- server/djangoapp/populate.py
+++ server/djangoapp/populate.py
@@ -8,41 +8,36 @@ def initiate():
         {"name":"Kia", "description":"Great cars. Korean technology"},
         {"name":"Toyota", "description":"Great cars. Japanese technology"},
         ]
-
-
-car_make_instances = []
-for data in car_make_data:
-    car_make_instances.append(
-        CarMake.objects.create(
-            name=data['name'],
-            description=data['description']
+    car_make_instances = []
+    for data in car_make_data:
+        car_make_instances.append(
+            CarMake.objects.create(
+                name=data['name'],
+                description=data['description']
+            )
         )
-    )
-
-# Create CarModel instances with dealer_id included
-car_model_data = [
-    {"name":"Pathfinder","type":"SUV","year":2023,"car_make":car_make_instances[0],"dealer_id":1},
-    {"name":"Qashqai","type":"SUV","year":2023,"car_make":car_make_instances[0],"dealer_id":1},
-    {"name":"XTRAIL","type":"SUV","year":2023,"car_make":car_make_instances[0],"dealer_id":1},
-    {"name":"A-Class","type":"SUV","year":2023,"car_make":car_make_instances[1],"dealer_id":2},
-    {"name":"C-Class","type":"SUV","year":2023,"car_make":car_make_instances[1],"dealer_id":2},
-    {"name":"E-Class","type":"SUV","year":2023,"car_make":car_make_instances[1],"dealer_id":2},
-    {"name":"A4","type":"SUV","year":2023,"car_make":car_make_instances[2],"dealer_id":3},
-    {"name":"A5","type":"SUV","year":2023,"car_make":car_make_instances[2],"dealer_id":3},
-    {"name":"A6","type":"SUV","year":2023,"car_make":car_make_instances[2],"dealer_id":3},
-    {"name":"Sorrento","type":"SUV","year":2023,"car_make":car_make_instances[3],"dealer_id":4},
-    {"name":"Carnival","type":"SUV","year":2023,"car_make":car_make_instances[3],"dealer_id":4},
-    {"name":"Cerato","type":"SEDAN","year":2023,"car_make":car_make_instances[3],"dealer_id":4},
-    {"name":"Corolla","type":"SEDAN","year":2023,"car_make":car_make_instances[4],"dealer_id":5},
-    {"name":"Camry","type":"SEDAN","year":2023,"car_make":car_make_instances[4],"dealer_id":5},
-    {"name":"Kluger","type":"SUV","year":2023,"car_make":car_make_instances[4],"dealer_id":5},
-]
-
-for data in car_model_data:
-    CarModel.objects.create(
-        name=data['name'],
-        car_make=data['car_make'],
-        dealer_id=data['dealer_id'],
-        type=data['type'],
-        year=data['year']
-    )
+    car_model_data = [
+        {"name":"Pathfinder","type":"SUV","year":2023,"car_make":car_make_instances[0],"dealer_id":1},
+        {"name":"Qashqai","type":"SUV","year":2023,"car_make":car_make_instances[0],"dealer_id":1},
+        {"name":"XTRAIL","type":"SUV","year":2023,"car_make":car_make_instances[0],"dealer_id":1},
+        {"name":"A-Class","type":"SUV","year":2023,"car_make":car_make_instances[1],"dealer_id":2},
+        {"name":"C-Class","type":"SUV","year":2023,"car_make":car_make_instances[1],"dealer_id":2},
+        {"name":"E-Class","type":"SUV","year":2023,"car_make":car_make_instances[1],"dealer_id":2},
+        {"name":"A4","type":"SUV","year":2023,"car_make":car_make_instances[2],"dealer_id":3},
+        {"name":"A5","type":"SUV","year":2023,"car_make":car_make_instances[2],"dealer_id":3},
+        {"name":"A6","type":"SUV","year":2023,"car_make":car_make_instances[2],"dealer_id":3},
+        {"name":"Sorrento","type":"SUV","year":2023,"car_make":car_make_instances[3],"dealer_id":4},
+        {"name":"Carnival","type":"SUV","year":2023,"car_make":car_make_instances[3],"dealer_id":4},
+        {"name":"Cerato","type":"SEDAN","year":2023,"car_make":car_make_instances[3],"dealer_id":4},
+        {"name":"Corolla","type":"SEDAN","year":2023,"car_make":car_make_instances[4],"dealer_id":5},
+        {"name":"Camry","type":"SEDAN","year":2023,"car_make":car_make_instances[4],"dealer_id":5},
+        {"name":"Kluger","type":"SUV","year":2023,"car_make":car_make_instances[4],"dealer_id":5},
+    ]
+    for data in car_model_data:
+        CarModel.objects.create(
+            name=data['name'],
+            car_make=data['car_make'],
+            dealer_id=data['dealer_id'],
+            type=data['type'],
+            year=data['year']
+        )
