0519fd75b73ad16d52fa8f7cd2df9e73f981211f
Suresh Kumar <96344350+SureshKumarKummari@users.noreply.github.com>
2026-03-05 11:05:38 +0530

    cmment

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,6 +1,8 @@
 import LoginPanel from "./components/Login/Login"
 import { Routes, Route } from "react-router-dom";
 import Dealers from './components/Dealers/Dealers';
+import Dealer from "./components/Dealers/Dealer"
+
 
 
 function App() {
@@ -8,6 +10,8 @@ function App() {
     <Routes>
       <Route path="/login" element={<LoginPanel />} />
         <Route path="/dealers" element={<Dealers/>} />
+  	<Route path="/dealer/:id" element={<Dealer/>} />
+
 
     </Routes>
   );
